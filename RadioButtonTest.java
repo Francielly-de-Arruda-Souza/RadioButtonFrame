05f90b07749bd662fb994074bd87a06501cd10eb
Francielly-de-Arruda-Souza <85981700+Francielly-de-Arruda-Souza@users.noreply.github.com>
2021-06-18 19:39:22 -0400

    Add files via upload

---
+++ RadioButtonTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package radiobuttontest;
+
+import javax.swing.JFrame;
+
+public class RadioButtonTest {
+
+ public static void main(String[] args) {
+  RadioButtonFrame radioButtonFrame = new RadioButtonFrame();
+  radioButtonFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+  radioButtonFrame.setSize( 300, 100 );
+  radioButtonFrame.setVisible( true );
+
+ }
+
+}
